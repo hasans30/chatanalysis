70e9dbdc9976f4e3cfbf17985e2a39e10b517def
hasan <hasan@live.in>
2021-04-26 00:14:21 +0000

    added way to use highchart in react

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,7 +1,34 @@
-import React, {memo} from 'react'
+import React, { memo, useEffect } from 'react';
 import ChartProps from './chart.types';
+import * as Highcharts from 'highcharts';
 
-export const Chart = memo<ChartProps>( ( {chartid, selectedFilter} )=>{
-    
-    return <div> render chart for {chartid} with filter {selectedFilter} </div>
+export const Chart = memo<ChartProps>(({ chartid, selectedFilter }) => {
+    // const content=<div> render chart for {chartid} with filter {selectedFilter} </div>;
+
+
+    console.log(`${chartid} ${selectedFilter}`);
+    useEffect(() => {
+        const options: Highcharts.Options = {
+            chart: {
+                renderTo: 'main',
+            },
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar']
+            },
+            series: [
+                {
+                    type: 'line',
+                    data: [{
+                        x: 1,
+                        y: 2
+                    }, {
+                        x: 2,
+                        y: 3
+                    }]
+                }
+            ]
+        }
+        Highcharts.chart(options);
+    }, []);
+    return <div id="main" > </div>;
 });
