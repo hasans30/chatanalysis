6791729c6e9f7c3e84bf05e5b0f8ccdc8c05f0ac
hasan <hasan@live.in>
2021-04-27 15:19:26 -0700

    fixed body-parser deprecated issue - replaced with express
    Author:    hasan <hasan@live.in>

--- api/index.js
+++ api/index.js
@@ -1,32 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3030;
-const programmingLanguagesRouter = require('./routes/programmingLanguages');
+const programmingLanguagesRouter = require("./routes/programmingLanguages");
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(
-  bodyParser.urlencoded({
+  express.urlencoded({
     extended: true,
   })
 );
 
-app.get('/', (req, res) => {
-  res.json({'message': 'ok'});
-})
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
 
-app.use('/programming-languages', programmingLanguagesRouter);
+app.use("/programming-languages", programmingLanguagesRouter);
 
 /* Error handler middleware */
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   console.error(err.message, err.stack);
-  res.status(statusCode).json({'message': err.message});
-
+  res.status(statusCode).json({ message: err.message });
 
   return;
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
