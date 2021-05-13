206683406873ccfb47bb5f8e920680a364c05204
hasan <hasan@live.in>
2021-05-13 22:03:25 +0000

    navbar2 feature

---
+++ src/components/navbar2/SidebarData.tsx
@@ -0,0 +1,22 @@
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+  {
+    icon: <IoIcons.IoIosPaper />,
+    cName: "nav-text",
+    title: 'Monthly count (compact)',
+    path: '/monthly-compact',
+  },
+  {
+    title: 'Monthly count (all)',
+    path: '/monthly-all',
+    icon: <IoIcons.IoIosPaper />,
+    cName: "nav-text",
+  },
+  {
+    title: 'Daily trend',
+    path: '/daily',
+    icon: <IoIcons.IoIosPaper />,
+    cName: "nav-text",
+  }
+];
