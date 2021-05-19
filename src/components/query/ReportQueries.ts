f1d3e16af0f566ed11cb166821d22d28180c1b75
hasan <hasan@live.in>
2021-05-19 18:47:22 +0000

    converted environment variable for api URL

--- src/components/query/ReportQueries.ts
+++ src/components/query/ReportQueries.ts
@@ -4,11 +4,19 @@
     DailyTotalTrend,
 }
 
+const host=process.env.REACT_APP_API_BASE||'';
+const org=process.env.REACT_APP_ORG||'';
+
+//TODO: move the year in url parameter
+const year=process.env.REACT_APP_YEAR||'';
+const baseURL=`${host}/${org}/${year}`;
+
+
 const Query = new Map(
     [
-        [ReportType.CompactMonthly,'/data/compact-monthly'],
-        [ReportType.AllMonthly,'/data/all-monthly'],
-        [ReportType.DailyTotalTrend,'/data/daily-total-trend/data.json']
+        [ReportType.CompactMonthly,`${baseURL}/data/compact-monthly`],
+        [ReportType.AllMonthly,`${baseURL}/data/all-monthly`],
+        [ReportType.DailyTotalTrend,`${baseURL}/data/daily-total-trend/data.json`]
     ]
 );
 
