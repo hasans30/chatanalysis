d97e6cea0a072340fa9d43305462cec8398ffd9a
hasan <hasan@live.in>
2021-05-25 05:28:09 +0000

    hide compact menu except for nhs group

--- src/components/navbar2/SidebarData.tsx
+++ src/components/navbar2/SidebarData.tsx
@@ -6,29 +6,34 @@ export const SidebarData = [
     path: '/report/monthly-all',
     icon: <IoIcons.IoIosPaper />,
     cName: "nav-text",
+    id: 1
   },
   {
     icon: <IoIcons.IoIosPaper />,
     cName: "nav-text",
     title: 'Monthly count (compact)',
     path: '/report/monthly-compact',
+    id: 2
   },
   {
     title: 'Daily trend',
     path: '/report/daily',
     icon: <IoIcons.IoIosPaper />,
     cName: "nav-text",
+    id: 3
   },
   {
     title: 'Word cloud',
     path: '/report/wordcloud',
     icon: <IoIcons.IoIosPaper />,
     cName: "nav-text",
+    id: 4
   },
   {
     title: 'Admin tasks',
     path: '/report/admin-reports',
     icon: <IoIcons.IoIosPaper />,
     cName: "nav-text",
+    id: 5
   }
 ];
